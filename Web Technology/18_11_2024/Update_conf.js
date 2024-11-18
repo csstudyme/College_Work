41ff944dd65c82f725acb61bc92ce538a3d70238
NCRD <NCRD@gmail.com>
2024-11-18 11:04:40 -0800

    18/11/2024

---
+++ Web Technology/18_11_2024/Update_conf.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql')
+var con = mysql.createConnection({
+host:'localhost',
+user:'root',
+password:'',
+database:'CONFERENCE'
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected...");
+
+sql = `UPDATE conference_data SET name= 'Datta' WHERE name = 'Mira'`;
+    con.query(sql, function(err, result){
+        if(err) throw err;
+
+        console.log(result)
+        con.end();
+    });
+
+});
